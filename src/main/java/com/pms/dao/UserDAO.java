4ce0e72df64bfd898ef90a86aa0d1a69512427fc
BYIRINGIRO-Samuel <byiringirosam08@gmail.com>
2026-03-18 18:54:57 +0100

    Expand UserDAO with CRUD methods

--- src/main/java/com/pms/dao/UserDAO.java
+++ src/main/java/com/pms/dao/UserDAO.java
@@ -1,8 +1,13 @@
 package com.pms.dao;
 
+import java.util.List;
 import com.pms.model.User;
 
 public interface UserDAO {
     void registerUser(User user);
     User authenticateUser(String email, String password);
+    List<User> getAllUsers();
+    void deleteUser(Long id);
+    void updateUser(User user);
+    User getUserById(Long id);
 }
